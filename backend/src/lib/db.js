3eba5d055f4d64b153f14f22d337f5497e74796b
jayaram <jayaram8520@gmail.com>
2025-08-13 19:27:33 +0530

    projectcompleted

---
+++ backend/src/lib/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB=async()=>{
+    try{
+        const conn=await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MONGODB connected:${conn.connection.host}`);
+    }
+    catch(err)
+    {
+        console.log("MONGODB connection failed",err);
+    }
+}
